3eb46a25f4492f5d5f2db06a600a36d61d6b635e
ishaq4466 <smartbuddy1995@gmail.com>
2019-04-18 01:41:37 +0530

    ADded player and points list

--- dream.py
+++ dream.py
@@ -13,28 +13,71 @@ def genSet(A):
 #a=random.sample(range(1,10),22)
 #random.shuffle(a)
 #a=[float(x) for x in input().split()]
+# print(a.split())
 #print(a,a[:-1],a[-1:])
 #a=['a','b','c','d','e','f','g','f','h']
 a=[9.5,9.5,9,11,9.5,8.5,8.5,9.0,8.5,8.5,8.5]
+
 b=sorted(a,reverse=True)
 print("points:%s"%(a))
 print("points:%s"%(b))
-c=a+b
-print("points:%s"%(c))
+points=a+b
+names=["AmbatiRayudu",
+"FafDuPlessis",
+"KedarJadhav",
+"MuraliVijay",
+"SureshRaina",
+"DavidWilley",
+"DwayneBravo",
+"MitchellSantner",
+"RavindraJadeja",
+"ShaneWatson",
+"MSDhoni"
+]
+names+=[
+"ChrisLynn",
+"JoeDenly",
+"NitishRana",
+"RinkuSingh",
+"RobinUthappa",
+"ShubmanGill",
+"AndrRussell",
+"CarlosBrathwaie",
+"DineshKarthik",
+"NikhilNaik",
+"HarryGurney"
+]
 
-print("Total length of points: "+str(len(a+b)))
+print("points:%s"%(points))
+print(names)
+print("Total length of points: "+str(len(points)))
 print("============================================")
-list1=genSet(c)
+players={}
+for k,v in zip(names,points):
+	players[k]=v
 
-#print(list1)
+for k,v in players.items():
+	print("%s 				%0.1f"%(k,v))
+	
+print("============================================")
+
+list1=list(players.values())
+print(list1)	
+print("============================================")
+
+list2=genSet(list1)
+
+#print(list32)
 print("============================================")
 players=11
 point=100
 #print("No of players Required: %d \nRequired point<=%d"%(players,point))
 print("No of players: %d \nRequired point=%d"%(players,point))
-for elements in list1:
+for elements in list2:
 	if len(elements)==players and sum(elements)==point:
-		print("%s Tot_Points:[%d]"%(str(elements),sum(elements)))
-
+		#print("%s Tot_Points:[%d]"%(str(elements),sum(elements)))
+		for x in range(len(elements)):
+			print(players[elements[x]],end=",")
+		print("Tot_Points:[%d]"%(sum(elements)))
 
 
